775770fae124423ad9ecbf0e4a3741be2bf1b6b1
Carlos Loureda Parrado <balanzeneto@gmail.com>
2018-10-02 16:49:58 +0200

    feat: Added Pet component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Luna"),
+    React.createElement("h1", {}, "Dog"),
+    React.createElement("h1", {}, "Havanese")
+  ]);
+};
+
 const App = () => {
   return React.createElement(
     "div",
     {},
-    React.createElement("h1", {}, "Rescue me!")
+    React.createElement("h1", {}, "Rescue me!"), // children
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet)
   );
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
