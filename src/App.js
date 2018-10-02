e51159a865278364ae6f173a3fbfbf1d11b8b798
Carlos Loureda Parrado <balanzeneto@gmail.com>
2018-10-02 16:49:27 +0200

    Initial commit: React loaded as script without any magic

---
+++ src/App.js
@@ -0,0 +1,8 @@
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Rescue me!")
+  );
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
