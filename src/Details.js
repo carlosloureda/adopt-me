d2ca3924fd633c4b98fac83e9e69f2f3b52b4d5e
Carlos Loureda Parrado <balanzeneto@gmail.com>
2018-10-04 06:54:48 +0200

    refactor: added babel to be able to add top level attributes to class and avoid creating constructors

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,56 @@
 import React, { Component } from "react";
+import pf from "petfinder-client";
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 export class Details extends Component {
+  state = {
+    loading: true
+  };
+  componentDidMount() {
+    petfinder.pet
+      .get({
+        output: "full",
+        id: this.props.id
+      })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed = pet.breeds.breed;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        }
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(err => {
+        this.setState({ error: err });
+      });
+  }
   render() {
-    return <div>Details</div>;
+    if (this.state.loading) {
+      return <h1>loading ...</h1>;
+    }
+    const { animal, breed, location, description } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
